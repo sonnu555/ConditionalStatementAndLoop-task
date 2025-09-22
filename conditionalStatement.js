69bfce7867556a8fb841447d1099b5c20e251f45
sonnu <pandeysonnu@gmail.com>
2025-09-22 16:18:45 +0400

    first changed

---
+++ conditionalStatement.js
@@ -0,0 +1,542 @@
+// 1. Age Check: Write an `if-else` statement to check if a person is 18 years old
+//  or older. Print "Adult" if true, otherwise "Not an adult."
+// ans:
+// let age=18
+// if (age>=18){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Not an adult");
+// }
+
+// 2. Number Sign: Write an `if-else` statement to check if a number is positive.
+//  Print "Positive" if true, otherwise "Not positive."
+// ans:
+// let num=5
+// if (num>0){
+//     console.log("Positive number");
+// }
+// else{
+//     console.log("Not positive number");
+// }
+
+// 3. Even or Odd: Write an `if-else` statement to check if a number is even. 
+// Print "Even" if true, otherwise "Odd."
+// ans:
+// let x=8
+// if (x%2==0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd")
+// }
+
+// 4. Vowel or Consonant: Write an `if-else` statement to check if a given character
+//  is a vowel. Print "Vowel" if true, otherwise "Consonant."
+// Ans:
+// let letter='a';
+// if (letter == 'a' ||letter=='e'||letter=='i'||letter=='o'||letter=='u'){
+//     console.log('character is a vowel');
+// }
+// else{
+//     console.log('character is a consonant');
+// }
+
+// 5. Check Zero: Write an `if-else` statement to check if a number is zero. 
+// Print "Zero" if true, otherwise "Non-zero."
+// Ans:
+// let y=0
+// if (y === 0){
+//     console.log('Zero');
+// }
+// else
+//     console.log('Non-zero');
+
+// 6. Multiple of Five: Write an `if-else` statement to check if a number is a 
+// multiple of 5. Print "Multiple of 5" if true, otherwise "Not a multiple of 5."
+// Ans:
+// let a=39
+// if (a%5==0){
+//     console.log('multiple of 5');
+// }
+// else{
+//     console.log("Not a multiple of 5");
+// }
+
+// 7. Greater Than Ten: Write an `if-else` statement to check if a number is 
+// greater than 10. Print "Greater than 10" if true, otherwise "10 or less."
+// Ans:
+// let numb=11
+// if (numb>10){
+//     console.log("Greater than 10");
+// }
+// else{
+//     console.log("10 or less");
+// }
+    
+// 8. Check Equality: Write an `if-else` statement to check if two numbers are 
+// equal. Print "Equal" if true, otherwise "Not equal."
+// Ans::
+// let b=3
+// let c=5
+// if (b === c){
+//     console.log('Equal')
+// }
+// else{
+//     console.log("Not equal")
+// }
+
+// 9. String Length Check: Write an `if-else` statement to check if the length of 
+// a string is greater than 5. Print "Long string" if true, otherwise "Short string."
+// Ans:
+// let nam='sonu'
+// if (nam.length>5){
+//     console.log("Long string");
+// }
+// else{
+//     console.log("Short string")
+// }
+
+// 10. Check Uppercase: Write an `if-else` statement to check if a character is 
+// uppercase. Print "Uppercase" if true, otherwise "Lowercase."
+// Ans:
+// let letters='RAM'
+// if (letters === letters.toUpperCase()){
+//     console.log("Uppercase")
+// }
+// else{
+//     console.log("Lowercase")
+// }
+
+// 11. Check Weekend: Write an `if-else` statement to check if a day of the week 
+// is Saturday. Print "Weekend" if true, otherwise "Not weekend."
+// Ans:
+// let day='Saturday'
+// if (day==='Saturday'){
+//     console.log("Weekend");
+// }
+// else{
+//     console.log("Not weekend")
+// }
+
+// 12. Age Category: Write an `if-else` statement to check if a person is a child
+//  (under 12). Print "Child" if true, otherwise "Not a child."
+// Ans:
+// let agee=11
+// if (agee<12){
+//     console.log("Child");
+// }
+// else{
+//     console.log("Not a child")
+// }
+
+// 13. Temperature Check: Write an `if-else` statement to check if the temperature 
+// is above 30 degrees. Print "Hot" if true, otherwise "Cold."
+// Ans:
+// let temp=33
+// if (temp>30){
+//     console.log("Hot");
+// }
+// else{
+//     console.log("Cold")
+// }
+// 14. Number Range: Write an `if-else` statement to check if a number is between 
+// 1 and 100. Print "In range" if true, otherwise "Out of range."
+// Ans:
+// let number=100
+// if (number>=1 && number<=100){
+//     console.log("In range");
+// }
+// else{
+//     console.log("Out of range")
+// }
+// 15. Grade Check: Write an `if-else` statement to check if a student’s score is
+//  greater than or equal to 50. Print "Pass" if true, otherwise "Fail."
+// Ans:
+// let grade=50
+// if (grade>=50){
+//     console.log("Pass");
+// }
+// else{
+//     console.log("Fail")
+// }
+// 16. Divisibility Check: Write an `if-else` statement to check if a number is 
+// divisible by 3. Print "Divisible by 3" if true, otherwise "Not divisible by 3."
+// Ans:
+// let z=125139
+// if (z%3==0){
+//     console.log("Divisible by 3");
+// }
+// else{
+//     console.log("Not divisible by 3")
+// }
+// 17. Morning or Afternoon: Write an `if-else` statement to check if the time is 
+// before 12 PM. Print "Morning" if true, otherwise "Afternoon."
+// Ans:
+// let time=9      
+// if (time<12){
+//     console.log("Morning");
+// }
+// else{
+//     console.log("Afternoon");
+// }
+// 18. Teenager Check: Write an `if-else` statement to check if a person’s age is 
+// between 13 and 19. Print "Teenager" if true, otherwise "Not a teenager."
+// Ans:
+// let Age=16
+// if (Age>=13 && Age<=19){
+//     console.log("Teenager");
+// }
+// else{
+//     console.log("Not a teenager")
+// }
+// 19. String Content Check: Write an `if-else` statement to check if a string 
+// contains the letter "a". Print "Contains 'a'" if true, otherwise "Does not 
+// contain 'a'."
+// Ans:
+// let letter="sanu"
+// if (letter.includes('a')){
+//     console.log("Contains 'a'")
+// }
+// else{
+//     console.log("Does not contain 'a'")
+// }
+
+// 20. Leap Year Check: Write an `if-else` statement to check if a given year is a
+//  leap year. Print "Leap year" if true, otherwise "Not a leap year."
+// Ans:
+// let year=2016
+// if (year%4==0){
+//     console.log("Leap year")
+// }
+// else{
+//     console.log("Not a leap year")
+// }
+// 21. Check Negative: Write an `if-else` statement to check if a number is 
+// negative. Print "Negative" if true,  otherwise "Positive or zero."
+// Ans:
+// let p=-5
+// if(p<0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("Positive or zero")
+// }
+// 22. Password Match: Write an `if-else` statement to check if a user’s input 
+// matches the password "1234". Print "Access granted" if true, otherwise "Access 
+// denied."
+// Ans:
+// let pass="1234"
+// if (pass === "1234"){
+//     console.log("Access granted")
+// }
+// else{
+//     console.log("Access denied")
+// }
+// 23. Greater Number: Write an `if-else` statement to check if the first of two
+//  numbers is greater. Print  "First is greater" if true, otherwise "Second is 
+// greater or equal."
+// Ans:
+// let firstGreater=9
+// let secondGreater=6
+// if (firstGreater>secondGreater){
+//     console.log("First is greater")
+// }
+// else{
+//     console.log("Second is greater or equal")
+// }
+// 24. Prime Check: Write an `if-else` statement to check if a number is greater
+//  than 1 and divisible only by 1 and itself. Print "Prime" if true, otherwise
+//  "Not prime."
+
+// 25. Eligible for Discount: Write an `if-else` statement to check if a customer’s
+//  age is 65 or older. Print "Eligible for senior discount" if true, otherwise 
+// "Not eligible."
+// Ans:
+// let Agee=65
+// if (Agee>=65){
+//     console.log("Eligible for senior discount")
+// }
+// else{
+//     console.log("Not eligible")
+// }
+// 26. String Comparison: Write an `if-else` statement to check if two strings are 
+// exactly the same. Print "Match" if true, otherwise "Do not match."
+// Ans:
+// let firstName='sonu'
+// let secondName='sonu'
+// if (firstName === secondName){
+//     console.log("Match")
+// }
+// else{
+//     console.log("Do not match")
+// }
+
+// 27. Multiples of Two: Write an `if-else` statement to check if a number is a 
+// multiple of 2. Print "Multiple of 2" if true, otherwise "Not a multiple of 2."
+// Ans:
+// let m=11
+// if  (m%2==0){
+//     console.log("multiple of 2");
+// }
+// else{
+//     console.log("Not a multiple of 2")
+// }
+// 28. Driving License Check: Write an `if-else` statement to check if a person
+//  is old enough to get a driving license (age 18 or older). Print "Eligible"
+//  if true, otherwise "Not eligible."
+// Ans:
+// let ageLimit=18
+// if (ageLimit>=18){
+//     console.log("Eligible")
+// }
+// else{
+//     console.log("Not eligible")
+// }
+
+// 29. Letter Case Check: Write an `if-else` statement to check if a character is
+//  lowercase. Print "Lowercase" if true, otherwise "Uppercase."
+// Ans:
+// let letterCheck='sonu'
+// if (letterCheck ===letterCheck.toLowerCase()){
+//     console.log("Lowercase");
+// }
+// else{
+//     console.log("Uppercase");
+// }
+// 30. Number Check: Write an `if-else` statement to check if a number is a 
+// single-digit number. Print "Single-digit" if true, otherwise "More than one 
+// digit."
+// Ans:
+// let digit="5"
+// if (digit.length<=1){
+//     console.log("Single-digit")
+// }
+// else{
+//     console.log("More than one digit")
+// }
+// 31. Divisibility by 10: Write an `if-else` statement to check if a number is
+//  divisible by 10. Print "Divisible by 10" if true, otherwise "Not divisible by
+//  10."
+// Ans:
+// let n=50
+// if (n%10==0){
+//     console.log("Divisible by 10")
+// }
+// else{
+//     console.log("Not divisible by 10")
+// }
+// 32. Check Month: Write an `if-else` statement to check if a month number 
+// corresponds to a month with 31 days. Print "31 days" if true, otherwise 
+// "Not 31 days."
+// Ans:
+// let monthNumber = 1; 
+
+// const monthsWith31Days = [1, 3, 5, 7, 8, 10, 12];
+
+// if (monthsWith31Days.includes(monthNumber)) {
+//     console.log("31 days");
+// } else {
+//     console.log("Not 31 days");
+// }
+
+// 33. Password Length Check: Write an `if-else` statement to check if a password 
+// is at least 8 characters long. Print "Valid password" if true, otherwise 
+// "Password too short."
+// Ans:
+// let password='sonusonu'
+// if (password.length>=8){
+//     console.log("Valid password");
+// }
+// else{
+//     console.log("Password too short");
+// }
+// 34. Check for Zero: Write an `if-else` statement to check if a number is zero.
+//  Print "Zero" if true, otherwise "Not zero."
+// Ans:
+// let r=0
+// if (r === 0){
+//     console.log("Zero")
+// }
+// else{
+//     console.log("Not zero")
+// }
+// 35. Grade A Check: Write an `if-else` statement to check if a student’s score 
+// is 90 or above. Print "Grade A" if true, otherwise "Less than Grade A."
+// Ans:
+// let score=93
+// if(score>=90){
+//     console.log("Grade A");
+// }
+// else{
+//     console.log("Less than Grade A")
+// }
+// 36. Empty String Check: Write an `if-else` statement to check if a string is 
+// empty. Print "Empty" if true, otherwise "Not empty."
+// Ans:
+// let word=''
+// if (word ===''){
+//     console.log("Empty")
+// }
+// else{
+//     console.log("Not empty")
+// }
+// 37. Multiple of 4: Write an `if-else` statement to check if a number is a
+//  multiple of 4. Print "Multiple of 4" if true, otherwise "Not a multiple of 4."
+// Ans:
+// let t=16
+// if (t%4 == 0){
+//     console.log("Multiple of 4");
+// }
+// else{
+//     console.log("Not a multiple of 4");
+// }
+
+// 38. Day of the Week: Write an `if-else` statement to check if a given day is 
+// Sunday. Print "Sunday" if true, otherwise "Not Sunday."
+// Ans:
+// let Day="Sunday"
+// Day=Day.toLowerCase();
+
+// if(Day === 'sunday'){
+//     console.log("Sunday");
+// }
+// else{
+//     console.log("Not Sunday")
+// }
+
+// 39. String Starts with A: Write an `if-else` statement to check if a string 
+// starts with the letter "A". Print "Starts with A" if true, otherwise 
+// "Does not start with A."
+// Ans:
+// let startWord='Ansu'
+// if (startWord.charAt(0) === 'A'){
+//     console.log("Starts with A")
+// }
+// else{
+//     console.log("Does not start with A")
+// }
+// 40. Check for Space: Write an `if-else` statement to check if a string contains 
+// a space. Print "Contains space" if true, otherwise "No space."
+// Ans:
+// let input='so nu'
+// if (input.includes(" ")){
+//     console.log("Contains space");
+// }
+// else{
+//     console.log("No space")
+// }
+// 41. String Length Even: Write an `if-else` statement to check if the length of 
+// Print "Even length" if true, otherwise "Odd length."
+// a string is an even number. 
+// Ans:
+// let Input="sonu"
+// if (Input.length%2 == 0){
+//     console.log("Even length")
+// }
+// else{
+//     console.log("Odd length")
+// }
+// 42. Temperature Conversion: Write an `if-else` statement to check if a
+//  temperature in Celsius is above 100 degrees. Print "Above boiling point"
+//  if true, otherwise "Below boiling point."
+// Ans:
+// let tempe=103
+// if(tempe>100){
+//     console.log("Above boiling point");
+// }
+// else{
+//     console.log("Below boiling point");
+// }
+// 43. Work Hours Check: Write an `if-else` statement to check if the current 
+// time is during work hours (9 AM to 5 PM). Print "Working hours" if true, 
+// otherwise "Off hours."
+// Ans:
+// let Time=11
+// if (Time>=9 && Time<17){
+//     console.log("Working hour")
+// }
+// else{
+//     console.log("Off hours")
+// }
+
+// 44. BMI Check: Write an `if-else` statement to check if a person’s Body Mass 
+// Index (BMI) is in the  healthy range (18.5 to 24.9). Print "Healthy" if true,
+//  otherwise "Not healthy."
+// Ans:
+// let bmi=23
+// if (bmi>=18.5 && bmi<=24.9){
+//     console.log("Healthy")
+// }
+// else{
+//     console.log("Not healthy")
+// }
+
+// 45. Check Odd Number: Write an `if-else` statement to check if a number is odd.
+//  Print "Odd" if true, otherwise "Even."
+// Ans:
+// let o=2323232323
+// if (o%2 == 1){
+//     console.log("Odd")
+// }
+// else{
+//     console.log("Even")
+// }
+// 46. Check for Digit: Write an `if-else` statement to check if a character is
+//  a digit (0-9). Print "Digit" if true, otherwise "Not a digit."
+// Ans:
+// let Digit=5;
+// if ((Digit>=0  && Digit<=9)){
+//     console.log("Digit");
+// }
+// else{
+//     console.log("Not a digit");
+// }
+// 47. Check for Exclamation: Write an `if-else` statement to check if a string 
+// ends with an exclamation mark (!). Print "Exclamation mark" if true,
+//  otherwise "No exclamation mark."
+// Ans:
+// let str="morning!"
+// if (str.endsWith('!')){
+//     console.log("Exclamation mark");
+// }
+// else{
+//     console.log("No exclamation mark")
+// }
+
+// 48. Day Check: Write an `if-else` statement to check if today is a weekday
+//  (Monday to Friday). Print "Weekday" if true, otherwise "Weekend."
+// Ans:
+// let day="Monday";
+// if (day==="Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday" || day === "Friday"){
+//     console.log("Weekday");
+// }
+// else{
+//     console.log("Weekend");
+// }
+
+
+// 49. Check Square Number: Write an `if-else` statement to check if a number is a
+//  perfect square. Print "Perfect square" if true, otherwise "Not a perfect square."
+// Ans:
+// let number = 25; 
+
+// let sqrt = Math.sqrt(number); // Calculate the square root
+
+// if (Number.isInteger(sqrt)) {
+//     console.log("Perfect square");
+// } else {
+//     console.log("Not a perfect square");
+// }
+
+// 50. Temperature Range Check: Write an `if-else` statement to check if a 
+// temperature is between 15 and 25 degrees Celsius. Print "Comfortable" if true, 
+// otherwise "Uncomfortable.
+// Ans:
+// let tempr=23
+// if (tempr>=15 && tempr<=25){
+//     console.log("Comfortable")
+// }
+// else{
+//     console.log("Uncomfortable")
+// }
---
+++ loop.js
@@ -0,0 +1,156 @@
+// 1. Counting from 1 to 20: Write a loop to print numbers from 1 to 20.
+// Ans:
+// for(i=1;i<=20;i++){
+//     console.log(i);
+// }
+// 2. Sum of First 10 Numbers: Use a loop to calculate the sum of the first 
+// 10 natural numbers (1, 2, 3, ..., 
+// 10).
+// Ans:
+// let sum=0
+// for(i=1;i<=10;i++){
+//     sum +=i
+// }
+// console.log("the sum of first 10 natural number is:" + sum);
+
+// 3. Multiplication Table of 5: Create a loop to print the multiplication table
+//  of 5.
+// Ans:
+// let num=5
+// for(let i=1;i<=10;i++){
+//     console.log(`${num}x${i}=${num*i}`);
+// }
+
+// 4. Print Numbers from 1 to 100: Write a loop to print numbers from 1 to 100.
+// Ans:
+// for(let i=1;i<=100;i++){
+//     console.log(i);
+// }
+
+// 5. Sum of Odd Numbers: Use a loop to calculate the sum of odd numbers between
+//  1 and 10.
+// Ans:
+// let sum=0
+// for(let i=1;i<=10;i++){
+//     if (i%2 == 1)
+
+//     sum +=i
+// }
+// console.log("the sum of first odd natural number is:" + sum);
+// 6. Count Down from 20 to 1: Write a loop to print numbers from 20 down to 1.
+// Ans:
+
+// for(let i=20;i>=1;i--){
+//     console.log(i);
+// }
+// 7. Print Multiples of 3: Use a loop to print the first 10 multiples of 3.
+// Ans:
+// let num=3
+// for(let i=1;i<=10;i++){
+//     console.log(`${num}x${i}=${num*i}`)
+// }
+// 8. Print Squares of Numbers: Write a loop to print the square of each number
+//  from 1 to 10.
+// Ans:
+
+// for(let i=1;i<=10;i++){
+//     console.log(i**2)
+// }
+
+// 9. Sum of First 100 Numbers: Use a loop to calculate the sum of the first 100
+//  natural numbers.
+// Ans:
+
+// sum=0;
+// for(let i=1;i<=100;i++){
+//     sum +=i;
+// }
+// console.log("Sum of First 100 Numbers:"+sum)
+
+// 10. Print Cube of Numbers: Write a loop to print the cube of each number from
+//  1 to 5.
+// Ans:
+
+// for (let i=1;i<=5;i++){
+//     console.log(i**3);
+// }
+// 11. Print Numbers in Reverse Order: Write a loop to print numbers from 50 down
+//  to 1.
+// Ans:
+
+// for (i=50;i>=1;i--){
+//     console.log(i)
+// }
+// 12. Count Down Odd Numbers: Create a loop to print odd numbers from 19 down
+//  to 1.
+// Ans:
+
+// let count=0;
+// for(let i=19;i>=1;i--){
+//     if (i%2 === 1){
+//     console.log(i)
+//     count++;
+// }
+// }
+// console.log("total odd numbers:",count);
+
+
+// 13. Sum of First 5 Odd Numbers: Use a loop to calculate the sum of the
+//  first 5 odd numbers (1, 3, 5, 7, 9).
+// Ans:
+// let sum=0;
+// for (let i=1;i<=9;i++){
+//     if(i%2 ===1){
+//         console.log(i);
+//         sum +=i;
+//     }
+// }
+// console.log("Sum of first 5 Odd Numbers:",sum);
+
+// 14. Print the First 10 Positive Integers: Write a loop to print the first 10
+//  positive integers.
+// Ans:
+
+// for (let i=1;i<=10;i++){
+//     console.log(i)
+// }
+// 15. Print Powers of 2: Use a loop to print the powers of 2 from `2^1` to `2^10`.
+// Ans:
+// let n=2
+// for (let i=1;i<=10;i++){
+//     console.log(n**i);
+// }
+
+// 16. Sum of First 10 Even Numbers: Write a loop to calculate the sum of the 
+// first 10 even numbers (2, 4, 6, 
+// ..., 20).
+// Ans:
+// let sum=0;
+// for(let i=2;i<=20;i++){
+//     if(i%2 === 0){
+//         console.log(i);
+//         sum +=i;
+//     }
+// }
+// console.log("sum of first 10 even numbers:",sum);
+
+// 17. Print Multiples of 4: Create a loop to print the first 10 multiples of 4.
+// Ans:
+// n=4;
+// for(let i=1;i<=10;i++){
+//     console.log(`${n}x${i}=${n*i}`);
+// }
+// 18. Print First 5 Prime Numbers: Write a loop to print the first 5 prime numbers.
+// Ans:
+// for (leti=2;i<=5;i++){
+//     console.log
+// }
+// 19. Reverse Counting Even Numbers: Use a loop to print even numbers from 30 
+// down to 10.
+// Ans:
+
+// for (let i=30;i>=10;i--){
+//     if (i%2 === 0){
+//         console.log(i)
+//     }
+// }
